63adce91c4c3ab8abffb2c0c8bf095d928e3ec1a
Yosif Georgiev <iodesign@live.com>
2023-01-11 20:39:36 +0200

    NEW: add inline style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import "./App.css";
 
 function App() {
+
+const wrapper ={
+  display: 'flex',
+  justifyContent: 'space-between',
+  margin: '1rem',
+  paddingBottom: '1rem',
+  borderBottom: '1px solid #ccc',
+}
+
+const mealHeading ={
+  margin: '0 0 0.25rem 0'
+}
+
+const description ={
+  fontStyle: 'italic'
+}
+
+const price ={
+  marginTop: '0.25rem',
+  fontWeight: 'bold',
+  color: '#ad5502',
+  fontSize: '1.25rem'
+}
+
   return (
     <div className="App">
-      <div className="wrapper">
-        <h3 className="mealHeading">Sushi</h3>
-        <div className="description">I prefer steak than sushi.</div>
-        <div className="price">$9.99</div>
+      <div style={wrapper}>
+        <h3 style={mealHeading}>Sushi</h3>
+        <div style={description}>I prefer steak than sushi.</div>
+        <div style={price}>$9.99</div>
       </div>
     </div>
   );
